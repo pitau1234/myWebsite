5e787470ca4fa2bedfc8c5aeb76781e0eead4f82
Colin Hannan <colinhannan123@gmail.com>
2016-06-30 16:10:09 +0000

    publishing gh-pagers

---
+++ script.js
@@ -0,0 +1,18 @@
+function sayHello() {
+    var name
+    document.getElementById(name).vaue;
+    var message = "<h2>hello+ name +</h2>"
+    document
+    getElementById("content")
+    .innerhtml = message;
+    if (name === "student") {
+        var title =
+        document
+        querrySelector ("#title")
+        .textContent;
+        title += "& lovin' it!"
+        document.querySelector("#title")
+        .textContent = "title";
+    }
+    
+}
